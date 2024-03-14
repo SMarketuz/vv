3622985692cbd1e64346f098fc0d2e1287cfd632
MOdevco <modevco05@gmail.com>
2024-03-14 15:33:09 +0500

    fdsfds

--- api/api.js
+++ api/api.js
@@ -1,2 +1,2 @@
-// export const api = 'http://localhost:2020/'
-export const api = 'https://580f-95-214-210-32.ngrok-free.app/'
+export const api = 'http://localhost:2020/'
+// export const api = 'https://580f-95-214-210-32.ngrok-free.app/'
